cf19ddde1b6f0800a6fbda3d7f57c592bec1cfc8
M. Danish Wafi <mdanishwafinurrashid@gmail.com>
2026-04-11 23:56:57 +0800

    Muhem

--- pages/input.py
+++ pages/input.py
@@ -5,7 +5,7 @@ with st.container():
     
     def show():
         
-        st.title("EcoStep 🍂")
+        st.title("EcoStep Mulhim 🍂")
         st.caption("Calculate your CO2 daily emission")
         st.markdown("---")
         
